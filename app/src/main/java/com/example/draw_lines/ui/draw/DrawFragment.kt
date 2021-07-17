e09e646159ae735b0f1136a43b0f4683da1e7b7c
Strzelczyn <Strzelczyn15@gmail.com>
2021-07-17 18:02:18 +0200

    Draw lines

--- app/src/main/java/com/example/draw_lines/ui/draw/DrawFragment.kt
+++ app/src/main/java/com/example/draw_lines/ui/draw/DrawFragment.kt
@@ -1,23 +1,80 @@
 package com.example.draw_lines.ui.draw
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.draw_lines.R
+import com.example.draw_lines.databinding.FragmentDrawBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class DrawFragment : Fragment() {
 
+    companion object {
+        private const val AXIS_LIMIT = 5.0
+        private const val LEGEND_POSITION = 50
+    }
 
+    private var _binding: FragmentDrawBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: DrawViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_draw, container, false)
+    ): View {
+        _binding = FragmentDrawBinding.inflate(layoutInflater, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val graph = binding.graph
+        graph.viewport.isXAxisBoundsManual = true
+        graph.viewport.setMinX(-AXIS_LIMIT)
+        graph.viewport.setMaxX(AXIS_LIMIT)
+        graph.viewport.isYAxisBoundsManual = true
+        graph.viewport.setMinY(-AXIS_LIMIT)
+        graph.viewport.setMaxY(AXIS_LIMIT)
+        graph.legendRenderer.isVisible = true
+        graph.legendRenderer.backgroundColor = resources.getColor(android.R.color.white)
+        graph.legendRenderer.setFixedPosition(LEGEND_POSITION, LEGEND_POSITION)
+        registerObserver()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    private fun registerObserver() {
+        viewModel.callDraw.observe(viewLifecycleOwner, {
+            CoroutineScope(IO).launch {
+                val series = viewModel.formatData()
+                if (series != null) {
+                    binding.graph.addSeries(
+                        series
+                    )
+                } else {
+                    withContext(Main) {
+                        Toast.makeText(
+                            context,
+                            resources.getText(R.string.bad_data),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        })
+    }
 }
--- app/src/main/java/com/example/draw_lines/ui/draw/DrawViewModel.kt
+++ app/src/main/java/com/example/draw_lines/ui/draw/DrawViewModel.kt
@@ -1,7 +1,61 @@
 package com.example.draw_lines.ui.draw
 
+import android.graphics.Color
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.draw_lines.utils.SingleLiveEvent
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import java.util.*
 
 class DrawViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    companion object {
+        private const val AXIS_LIMIT = 6.0
+        private const val ALFA_COLOR = 255
+        private const val COLOR = 206
+        private const val STATIC_COLOR = 50
+    }
+
+    private val _callDraw = SingleLiveEvent<Boolean>()
+    val callDraw: LiveData<Boolean> get() = _callDraw
+
+    val valueA = MutableLiveData<String>()
+    val valueB = MutableLiveData<String>()
+
+    fun onClickCallDraw() {
+        _callDraw.postValue(true)
+    }
+
+    fun formatData(): LineGraphSeries<DataPoint>? {
+        return try {
+            drawLine(valueA.value?.toFloat()!!, valueB.value?.toFloat()!!)
+        } catch (nfe: NumberFormatException) {
+            null
+        }
+    }
+
+    private fun drawLine(valueA: Float, valueB: Float): LineGraphSeries<DataPoint> {
+        val series = LineGraphSeries(
+            arrayOf(
+                DataPoint(-AXIS_LIMIT, valueA * (-AXIS_LIMIT) + valueB),
+                DataPoint(AXIS_LIMIT, valueA * (AXIS_LIMIT) + valueB)
+            )
+        )
+        val rnd = Random()
+        series.color =
+            Color.argb(
+                ALFA_COLOR,
+                rnd.nextInt(COLOR) + STATIC_COLOR,
+                rnd.nextInt(COLOR) + STATIC_COLOR,
+                rnd.nextInt(COLOR) + STATIC_COLOR
+            )
+        if (valueB < 0) {
+            series.title = "${valueA}x${valueB}"
+        } else {
+            series.title = "${valueA}x+${valueB}"
+        }
+        return series
+    }
 }
